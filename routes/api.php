54dc4366816b993d8afa74fa0fb35747963e66b2
Angel Antezana <ghehahanghel@gmail.com>
2018-08-18 14:33:40 -0400

    Creating routes for ventas, clientes and productos

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,30 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
 
 Route::post('/auth/login', 'Api\AuthController@login');
 Route::post('/auth/register', 'Api\AuthController@register');
 
-Route::group(['middleware' => 'auth:api'], function (){
+Route::group(['middleware' => 'auth:api'], function () {
   Route::get('/auth/details', 'Api\AuthController@details');
 });
+/*Productos*/
+Route::get('/productos', 'ProductoController@getAll');
+Route::get('/productos', 'ProductoController@getById');
+Route::post('/productos', 'ProductoController@create');
+Route::put('/productos', 'ProductoController@edit');
+Route::delete('/productos', 'ProductoController@delete');
+/*Clientes*/
+Route::get('/clientes', 'ClienteController@getAll');
+Route::get('/clientes', 'ClienteController@getById');
+Route::post('/clientes', 'ClienteController@create');
+Route::put('/clientes', 'ClienteController@edit');
+Route::delete('/clientes', 'ClienteController@delete');
+/*Ventas*/
+Route::get('/ventas', 'VentaController@getAll');
+Route::get('/ventas', 'VentaController@getById');
+Route::post('/ventas', 'VentaController@create');
+Route::put('/ventas', 'VentaController@edit');
+Route::delete('/ventas', 'VentaController@delete');
